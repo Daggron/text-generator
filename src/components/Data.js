57b792ce6b130970fcf179811e4afb4c25992502
Daggron <returnofking04@gmail.com>
2019-10-07 19:14:32 +0530

    New Done

---
+++ src/components/Data.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Axios from 'axios'
+import Output from './output'
+class Data extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            html:true,
+            para:3,
+            text:''
+        }
+    }
+
+    getData(){
+        Axios.get('https://cors-anywhere.herokuapp.com/http://hipsterjesus.com/api?paras='+this.state.para+'&html='+this.state.html)
+        .then(response=>{
+            this.setState({
+                text:response.data.text
+            },()=>{
+                console.log(this.state);
+            })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+
+    
+
+    componentDidMount(){
+        this.getData()
+    }
+
+    
+    
+    shouldComponentUpdate(){
+        return true;
+    }
+
+    render(){
+      
+        return (
+            <div>
+                <input type="number" value={this.state.para} onChange={ this.changeData} />
+                <Output value={this.state.text} />
+            </div>
+        )
+    }
+}
+
+export default Data;
